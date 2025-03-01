f2ba3beb50f3eaed3a33a9c751f8be4687e7ce36
Chaitanya Undale <137377022+chaitanya06041@users.noreply.github.com>
2025-03-01 19:07:21 +0530

    updated app.py

--- app.py
+++ app.py
@@ -6,17 +6,33 @@ if __name__ == '__main__':
     image = Image.open(image_path)
     text_content = image_utils.scan_image_for_text(image)
 
-    unmodified_words = text_utils.string_tokenizer(text_content["unmodified"])
-    grayscaled = text_utils.string_tokenizer(text_content["auto_rotate"])
-    auto_rotate = text_utils.string_tokenizer(text_content["grayscaled"])
-    monochromed = text_utils.string_tokenizer(text_content["monochromed"])
-    mean_threshold = text_utils.string_tokenizer(text_content["mean_threshold"])
-    gaussian_threshold = text_utils.string_tokenizer(text_content["gaussian_threshold"])
-    deskewed_1 = text_utils.string_tokenizer(text_content["deskewed_1"])
-    deskewed_2 = text_utils.string_tokenizer(text_content["deskewed_2"])
-    deskewed_3 = text_utils.string_tokenizer(text_content["deskewed_3"])
+    
+    image_text_unmodified = text_content["unmodified"]
+    image_text_auto_rotate = text_content["auto_rotate"]
+    image_text_grayscaled = text_content["grayscaled"]
+    image_text_monochromed = text_content["monochromed"]
+    image_text_mean_threshold = text_content["mean_threshold"]
+    image_text_gaussian_threshold = text_content["gaussian_threshold"]
+    image_text_deskewed_1 = text_content["deskewed_1"]
+    image_text_deskewed_2 = text_content["deskewed_2"]
+    image_text_deskewed_3 = text_content["deskewed_3"]
 
-    print(unmodified_words, '\n', grayscaled, '\n', auto_rotate, '\n', monochromed, '\n', mean_threshold, '\n'
-          , gaussian_threshold, '\n', deskewed_1, '\n', deskewed_2, '\n', deskewed_3,'\n')
+    unmodified_words = text_utils.string_tokenizer(image_text_unmodified)
+    grayscaled = text_utils.string_tokenizer(image_text_auto_rotate)
+    auto_rotate = text_utils.string_tokenizer(image_text_grayscaled)
+    monochromed = text_utils.string_tokenizer(image_text_monochromed)
+    mean_threshold = text_utils.string_tokenizer(image_text_mean_threshold)
+    gaussian_threshold = text_utils.string_tokenizer(image_text_gaussian_threshold)
+    deskewed_1 = text_utils.string_tokenizer(image_text_deskewed_1)
+    deskewed_2 = text_utils.string_tokenizer(image_text_deskewed_2)
+    deskewed_3 = text_utils.string_tokenizer(image_text_deskewed_3)
+
+    original = image_text_unmodified + "\n" + image_text_auto_rotate + "\n" + image_text_grayscaled + "\n" + image_text_monochromed + "\n" + image_text_mean_threshold + "\n" + image_text_gaussian_threshold + "\n" + image_text_deskewed_1 + "\n" + image_text_deskewed_2 + "\n" +  image_text_deskewed_3
+
+    intelligible = unmodified_words + grayscaled + auto_rotate + monochromed + mean_threshold + gaussian_threshold + deskewed_1 + deskewed_2 + deskewed_3
+
+    print(original)
+    print('\n\n')
+    print(intelligible)
     
 
