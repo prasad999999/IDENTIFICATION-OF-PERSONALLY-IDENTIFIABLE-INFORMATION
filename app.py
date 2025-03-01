6cb32d61e746aefbf4c79e14a831a40e014189c1
Chaitanya Undale <137377022+chaitanya06041@users.noreply.github.com>
2025-03-02 02:47:08 +0530

    Added Feature for masking image

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import image_utils
 from PIL import Image
-import re, json, nltk, itertools, spacy, difflib, math
+import re, json, nltk, itertools, spacy, difflib, math, cv2, easyocr
+import numpy as np
 from nltk import word_tokenize, pos_tag, ne_chunk
 from nltk.corpus import stopwords
 nltk_resources = ["punkt", "maxent_ne_chunker_tab", "words", "averaged_perceptron_tagger", "stopwords"]
@@ -87,12 +88,62 @@ def get_formatted_text_info(text):
     except Exception as e:
         return {"error": str(e)}
 
+def get_json_data(response):
+    cleaned_result = re.sub(r"```json|\n```", "", result).strip()
+    if cleaned_result and cleaned_result.strip():  # Ensure it's not empty
+        try:
+            result_json = json.loads(cleaned_result)  # Convert to Python dictionary
+            print("Parsed JSON:", result_json)
+            return result_json
+        except json.JSONDecodeError as e:
+            print("Error parsing JSON:", e)
+            result_json = {}  # Default to an empty dictionary
+            return result_json
+    else:
+        print("Error: Received empty or invalid JSON response")
+        result_json = {}
+        return result_json
+
+def mask_image(image_path, result_json):
+    image = cv2.imread(image_path)
+    # Sensitive information to mask
+    sensitive_info = [
+        result_json.get("name"), 
+        result_json.get("Document id"), 
+        result_json.get("phone_no"), 
+        result_json.get("address"), 
+        result_json.get("DOB"), 
+        result_json.get("Expiry Date")
+    ]
+    sensitive_info = [str(info) for info in sensitive_info if info is not None]
+
+
+    reader = easyocr.Reader(['en'])
+    ocr_results = reader.readtext(image)
+
+    for (bbox, text, prob) in ocr_results:
+        normalized_text = text.replace(" ", "")  # Normalize OCR-detected text
+
+        if any(s in normalized_text for s in sensitive_info):  # Match normalized text
+            # Get bounding box coordinates
+            (top_left, top_right, bottom_right, bottom_left) = bbox
+            x_min = int(top_left[0])
+            y_min = int(top_left[1])
+            x_max = int(bottom_right[0])
+            y_max = int(bottom_right[1])
+            
+            # Apply a black mask over the detected text
+            cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 0, 0), -1)
+    cv2.imwrite("masked_document.jpg", image)
+    print("Masked Image Saved")
+
 
 if __name__ == '__main__':
     image_path = './Dummy/image.png'
     image = Image.open(image_path)
     original = extract_text_from_image(image)
     result = get_formatted_text_info(original)
-    print(result)
+    json_data = get_json_data(result)
+    mask_image(image_path, json_data)
     
 
